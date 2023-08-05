ee6ceeb8f38320aab5a3751f972c43194e2791b8
soyeba3 <soyebchandani@gmail.com>
2023-08-06 00:08:57 +0600

    Complete design about and contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import aboutImage from "public/about.jpeg";
 import styles from "./about.module.css";
 
 const About = () => {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={aboutImage} fill={true} alt="img" className={styles.img} />
+        <div className={styles.imgText}>
+          <h1>Digital Storytellers</h1>
+          <h2>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1>Who Are We?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1>What We Do?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+            Creative Illustrations
+            <br />
+            <br /> - Dynamic Websites
+            <br />
+            <br /> - Fast and Handy
+            <br />
+            <br /> - Mobile Apps
+          </p>
+          <Link href="/contact" className={styles.button}>
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import contactImg from "public/contact.png";
 import styles from "./contact.module.css";
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src={contactImg} alt="" className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
